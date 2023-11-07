f65845c9bd4e852a5b612017056840a9c3173092
Lucas Del Vecchio <lcs999999999@hotmail.com>
2023-11-07 10:21:11 -0300

    Incluido novo serviço de envio de email

--- src/main/java/local/alua/NewOrderMain.java
+++ src/main/java/local/alua/NewOrderMain.java
@@ -1,5 +1,6 @@
 package local.alua;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -13,13 +14,18 @@ public class NewOrderMain {
         var producer = new KafkaProducer<String, String>(properties());
         var value = "mensagem do programinha tosquinho";
         var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-        producer.send(record, (data, ex) ->{
+        Callback callback = (data, ex) -> {
+//        producer.send(record, (data, ex) ->{
             if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
             System.out.println("topic{"+data.topic()+"}/partition{"+data.partition()+"}/offset{"+data.offset()+"}/timestamp{" + data.timestamp()+"}");
-        }).get();
+        };
+        var email = "Estamos processando sua compraaa";
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL",email,email);
+        producer.send(record, callback).get();
+        producer.send(emailRecord,callback).get();
     }
 
     private static Properties properties() {
