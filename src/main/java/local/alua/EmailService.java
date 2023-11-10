41f8576c028a672cbb4d3b02155cff3ed6d565ef
Lucas Del Vecchio <lcs999999999@hotmail.com>
2023-11-10 12:13:10 -0300

    Criando serviços genéricos e migrando EmailService

--- src/main/java/local/alua/EmailService.java
+++ src/main/java/local/alua/EmailService.java
@@ -1,49 +1,30 @@
 package local.alua;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
-import java.util.UUID;
 
 public class EmailService {
 
     public static void main(String[] args){
-        var consumer = new KafkaConsumer<String, String>(properties());
-        consumer.subscribe(Collections.singleton("ECOMMERCE_SEND_EMAIL"));
-        while(true) {
-            var records = consumer.poll(Duration.ofMillis(100));
+        var emailService = new EmailService();
+        var service = new KafkaService("ECOMMERCE_SEND_EMAIL",
+                emailService::parse);
+        service.run();
+    }
 
-            if (!records.isEmpty()) {
-                System.out.println("Encontrado " + records.count() + " registros");
-                for (var record : records) {
-                    System.out.println("########################################################################################");
-                    System.out.println("Mandando email ");
-                    System.out.println(record.key());
-                    System.out.println(record.value());
-                    System.out.println(record.partition());
-                    System.out.println(record.offset());
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println("Email enviado!");
-                }
-            }
+    private void parse(ConsumerRecord<String,String> record){
+        System.out.println("########################################################################################");
+        System.out.println("Mandando email ");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        System.out.println("Email enviado!");
     }
 
-    private static Properties properties(){
-        var properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
-        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, EmailService.class.getSimpleName() + " - " + UUID.randomUUID().toString());
-        return properties;
-    }
 }
