900f4bf78965e0e24dfa1123897c0352d1556471
Lucas Del Vecchio <lcs999999999@hotmail.com>
2023-11-10 14:58:31 -0300

    Tornando propriedades genéricas

--- src/main/java/local/alua/KafkaService.java
+++ src/main/java/local/alua/KafkaService.java
@@ -13,9 +13,9 @@ class KafkaService {
     private final KafkaConsumer<String, String> consumer;
     private final ConsumerFunction parse;
 
-    KafkaService(String topic, ConsumerFunction parse){
+    KafkaService(String groupId, String topic, ConsumerFunction parse){
         this.parse = parse;
-        this.consumer = new KafkaConsumer<String, String>(properties());
+        this.consumer = new KafkaConsumer<>(properties(groupId));
         consumer.subscribe(Collections.singleton(topic));
     }
 
@@ -32,13 +32,13 @@ class KafkaService {
         }
     }
 
-    private static Properties properties(){
+    private static Properties properties(String groupId){
         var properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
-        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, EmailService.class.getSimpleName() + " - " + UUID.randomUUID().toString());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
         return properties;
     }
 }
