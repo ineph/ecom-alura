88e2b9a1ab53a043686be5e4124059181012a919
Lucas Del Vecchio <lcs999999999@hotmail.com>
2023-11-13 11:02:24 -0300

    Criando serializador para mensagem

--- src/main/java/local/alua/NewOrderMain.java
+++ src/main/java/local/alua/NewOrderMain.java
@@ -1,20 +1,25 @@
 package local.alua;
 
 
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        try (var dispatcher = new KafkaDispatcher()) {
+        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
+            try (var emailDispatcher = new KafkaDispatcher<String>()) {
 
-            for (var i = 0; i < 10; i++) {
-                var key = UUID.randomUUID().toString();
-                var value = key + "MENSAGEM DE NOVA COMPRA!";
-                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+                for (var i = 0; i < 10; i++) {
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = new BigDecimal(Math.random() * 5000 + 1);
+                    var order = new Order(userId, orderId, amount);
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
 
-                var email = "Estamos processando sua compraaa";
-                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+                    var email = "Estamos processando sua compraaa";
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+                }
             }
         }
     }
